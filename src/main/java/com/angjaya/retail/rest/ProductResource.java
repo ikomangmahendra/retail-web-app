1a2a40d96ed6b5f317c8ac697f49102da4bdac0d
hendra <hendra@xtramilesolutions.com.au>
2026-01-10 20:31:11 +0800

    Extend Product REST endpoint with CRUD operations: add methods for fetching by ID, creating, updating, and deleting products.

--- src/main/java/com/angjaya/retail/rest/ProductResource.java
+++ src/main/java/com/angjaya/retail/rest/ProductResource.java
@@ -3,9 +3,17 @@ package com.angjaya.retail.rest;
 import com.angjaya.retail.domain.Product;
 import com.angjaya.retail.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -21,5 +29,46 @@ public class ProductResource {
     public ResponseEntity<Iterable<Product>> getAllProducts() {
         var results = productRepository.findAll();
         return ResponseEntity.ok().body(results);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+        Optional<Product> optProduct = productRepository.findById(id);
+        if (optProduct.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok().body(optProduct.get());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        var result = productRepository.save(product);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
+        boolean isProductExist = productRepository.existsById(id);
+        if (!isProductExist) {
+            throw new RuntimeException("Product not found");
+        }
+
+        if (product.getId() == null) {
+            throw new RuntimeException("Product id cannot be null");
+        }
+
+        if (!Objects.equals(id, product.getId())) {
+            throw new RuntimeException("Product id must be same as path variable");
+        }
+
+        var result = productRepository.save(product);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
+        productRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
